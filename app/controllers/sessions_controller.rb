b103c84e410502650c4cbb47820d1e34d2ffbbf6
tam9191 <m.t.habite@gmail.com>
2019-10-03 10:37:32 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) 
+      flash[:success] = "ログインしました。"
       redirect_to user
     else
       flash.now[:danger] = "認証に失敗しました。"
@@ -14,8 +16,9 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
-    flash[:success] = "ログアウトしました"
+    log_out if logged_in?
+    flash[:success] = "ログアウトしました。"
     redirect_to root_url
   end
+  
 end
