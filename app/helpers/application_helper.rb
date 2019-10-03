1a33196116e86ab2e58850dc41a554d73dadc942
tam9191 <m.t.habite@gmail.com>
2019-10-03 01:31:53 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "")
+    basic_title = AttendanceApp
+    if page_name.empty?
+      basic_title
+    else
+      page_name + " | " + basic_title
+    end
+  end    
+    
 end
